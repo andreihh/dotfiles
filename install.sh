8bb4ce66e6c5dc914a0f63ddc00bf29fb3035ca3
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2017-10-22 17:14:58 +0300

    Added plantuml and optimize imports for IDEA.

--- install.sh
+++ install.sh
@@ -43,6 +43,7 @@ packages=\
 "python python3 python-dev python3-dev pylint pylint3 openjdk-8-jdk visualvm "\
 "vim "\
 "texlive texlive-latex-extra texlive-bibtex-extra latexmk "\
+"graphviz plantuml "\
 "libstdc++6:i386 libgcc1:i386 zlib1g:i386 libncurses5:i386"
 
 echo "Setting up backup directory '$backup_dir'..." && \
