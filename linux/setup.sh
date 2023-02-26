58f7e9f5795f9227529e23866a70721a931c0e4a
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2023-02-26 06:32:35 +0100

    Add Dell setup script and TLP config.

--- linux/setup.sh
+++ linux/setup.sh
@@ -11,7 +11,7 @@ function echoerr() {
 }
 
 packages=\
-"linux-tools-common util-linux lm-sensors smartmontools stress-ng "\
+"linux-tools-common util-linux tlp lm-sensors smartmontools stress-ng "\
 "vim git "\
 "openjdk-18-jdk visualvm "\
 "build-essential cmake "\
@@ -21,8 +21,10 @@ packages=\
 "graphviz plantuml "\
 "zenity ffmpeg vlc keepassxc "
 
+
 echo -e "\nUpdating package index..."
-sudo apt-get update \
+sudo add-apt-repository ppa:linrunner/tlp \
+  && sudo apt-get update \
   && echo "Package index updated!" \
   || echorr "Failed to update package index!"
 
@@ -45,6 +47,29 @@ gsettings set org.gnome.desktop.input-sources xkb-options \
   "['caps:swapescape']" \
   && echo "Caps Lock and Esc keys swapped!" \
   || echoerr "Failed to swap Caps Lock and Esc keys!"
+
+echo -e "\nConfiguring TLP settings..."
+TLP_CONFIG=$(sudo tlp stat | grep "Configured Settings:" | sed 's/.*: //')
+
+# Configures the given TLP option with the given value.
+function set_tlp_option() {
+  local option="$1"
+  local value="$2"
+  sudo sed -i -E "s/#?${option}=.*/${option}=${value}/" "$TLP_CONFIG"
+}
+
+[[ ! -z "$TLP_CONFIG" && -f "$TLP_CONFIG" ]] \
+  && set_tlp_option "START_CHARGE_THRESH_BAT0" "70" \
+  && set_tlp_option "STOP_CHARGE_THRESH_BAT0" "80" \
+  && set_tlp_option "CPU_SCALING_GOVERNOR_ON_AC" "powersave" \
+  && set_tlp_option "CPU_SCALING_GOVERNOR_ON_BAT" "powersave" \
+  && set_tlp_option "CPU_MAX_PERF_ON_AC" "80" \
+  && set_tlp_option "CPU_MAX_PERF_ON_BAT" "60" \
+  && set_tlp_option "CPU_BOOST_ON_AC" "1" \
+  && set_tlp_option "CPU_BOOST_ON_BAT" "0" \
+  && sudo systemctl enable tlp.service \
+  && echo "TLP configured successfully!" \
+  || echoerr "Failed to configure TLP!"
 
 echo -e "\nMaking hidden startup applications visible..."
 sudo sed -i "s/NoDisplay=true/NoDisplay=false/g" /etc/xdg/autostart/*.desktop \
