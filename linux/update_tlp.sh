7018cbaf694322971dfeff350024105d9d63727d
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2023-02-26 18:34:27 +0100

    Fix TLP updater.

--- linux/update_tlp.sh
+++ linux/update_tlp.sh
@@ -8,7 +8,7 @@ readonly USER_TLP_CONFIG="$HOME/.tlp.conf"
 [[ $# -gt 0 ]] && echo "Usage: $0" && exit 1
 
 [[ -f "$TLP_CONFIG" && -f "$USER_TLP_CONFIG" ]] \
-  && sudo sed -i '/# ~/\.tlp\.conf:.*/,$d' "$TLP_CONFIG" \
+  && sudo sed -i '/# ~\/\.tlp\.conf:/,$d' "$TLP_CONFIG" \
   && cat "$USER_TLP_CONFIG" | sudo tee -a "$TLP_CONFIG" \
   && sudo systemctl enable tlp.service \
   && sudo tlp start
