b9795327f74d49a40c05ab0878ca902e02b1b6a2
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2023-03-09 15:57:01 +0100

    Fix linux installer.

--- install_linux.sh
+++ install_linux.sh
@@ -7,6 +7,7 @@
 readonly GH_ROOT="https://raw.githubusercontent.com/andreihh/.dotfiles/master"
 readonly INSTALL_SCRIPT_URL="$GH_ROOT/scripts/install.sh"
 readonly SCRIPTS_DIR="$HOME/.dotfiles/scripts"
+readonly BACKUP_SCRIPT="$SCRIPTS_DIR/backup.sh"
 readonly SCRIPTS=\
 "$SCRIPTS_DIR/setup_dotfiles.sh "\
 "$SCRIPTS_DIR/linux/setup_packages.sh "\
@@ -19,7 +20,10 @@ readonly SCRIPTS=\
 [[ $# -gt 0 ]] && echo "Usage: $0" && exit 1
 
 echo "Downloading installer..."
-wget "$GH_URL/scripts/install.sh" || curl -LO "$INSTALL_SCRIPT" || exit 1
+wget "$INSTALL_SCRIPT_URL" || curl -LO "$INSTALL_SCRIPT_URL" || exit 1
 
 echo -e "\nRunning installer..."
-chmod +x ./install.sh && ./install.sh $SCRIPTS && rm ./install.sh || exit 1
+chmod +x ./install.sh \
+  && ./install.sh "$BACKUP_SCRIPT" $SCRIPTS \
+  && rm ./install.sh \
+  || exit 1
