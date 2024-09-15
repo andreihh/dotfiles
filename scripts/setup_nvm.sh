5e635b6a428a5a14323e95386e7f6b573bdd575b
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2024-09-15 02:56:37 +0200

    Minor formatting in nvm setup script.

--- scripts/setup_nvm.sh
+++ scripts/setup_nvm.sh
@@ -4,10 +4,10 @@
 
 . "$HOME/.dotfiles/scripts/utils.sh" || exit 1
 
-[[ $# -gt 0 ]] && echo "Usage: $0" && exit 1
-
 readonly INSTALLER=\
 "https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh"
+
+[[ $# -gt 0 ]] && echo "Usage: $0" && exit 1
 
 echo "Installing latest LTS version of NVM..."
 PROFILE=~/.exports bash -c "curl -o- $INSTALLER | bash" \
