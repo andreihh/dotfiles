7697c58374d28c954277cce7a72f0f73f729ac54
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2024-10-05 19:36:20 +0200

    Update dotfiles installer to use stow and git to install and backup dotfiles.

--- installer/install_dotfiles.sh
+++ installer/install_dotfiles.sh
@@ -5,18 +5,26 @@
 # If installing a single file fails, aborts the installation and reports the
 # failure.
 
+readonly CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+readonly DOTFILES_HOME="$CONFIG_HOME/dotfiles"
+
 usage() {
   cat << EOF
-  Usage: $0 [-h] [-d] -o <operating-system>
+  Usage: $0 [-h] [-d] [-b <backup_dir>]
 
     -d  Debug / dry run mode (simulate all actions, but do not execute them).
+    -b  Backup directory where dotfiles should be copied to.
     -h  Print this message and exit.
 EOF
 }
 
-while getopts "dh" option; do
+while getopts "db:h" option; do
   case "$option" in
-    d) debug="-d" ;;
+    d)
+      debug="-d"
+      stow_opts="-nvv"
+      ;;
+    b) backup_dir="$OPTARG" ;;
     h) usage && exit 0 ;;
     *) usage && exit 1 ;;
   esac
@@ -25,11 +33,36 @@ done
 echo "Installing dotfiles..."
 [[ -n "$debug" ]] && echo "Running in debug mode!"
 
-for file in "$HOME/.dotfiles"/.[!.]*; do
-  [[ ! -f "$file" ]] && continue
-  echo "Installing dotfile '$file'..."
-  filename=$(basename "$file")
-  [[ -n "$debug" ]] || ln -sfv "$file" "$HOME/$filename"
-done
+if [[ ! -d "$DOTFILES_HOME" ]]; then
+  echo "Dotfiles repository '$DOTFILES_HOME' is missing!"
+  exit 1
+fi
+
+echo "Checking that the Git repository is clean..."
+if [[ $(git -C "$DOTFILES_HOME" status --porcelain 2> /dev/null) ]]; then
+  echo "You must commit changes to the Git repository and get to a clean state!"
+  [[ -n "$debug" ]] || exit 1
+fi
+
+if [[ -n "$backup_dir" ]]; then
+  if [[ -e "$backup_dir" ]]; then
+    echo "You must move the existing backup '$backup_dir' elsewhere!"
+    [[ -n "$debug" ]] || exit 1
+  fi
+
+  echo "Setting up backup directory '$backup_dir'..."
+  [[ -n "$debug" ]] || mkdir -p "$backup_dir"
+fi
+
+echo "Stowing dotfiles, adopting conflicting files..."
+stow $stow_opts --no-folding --adopt -t "$HOME" -d "$DOTFILES_HOME" .
+
+if [[ -n "$backup_dir" ]]; then
+  echo "Backing up dotfiles..."
+  [[ -n "$debug" ]] || cp -Prv "$DOTFILES_HOME" "$backup_dir"
+fi
+
+echo "Reverting changes from adopted files..."
+[[ -n "$debug" ]] || git -C "$DOTFILES_HOME" checkout .
 
 echo "Dotfiles installed successfully!"
