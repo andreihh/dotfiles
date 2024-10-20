6257394fd79d1b9cefa83a5999a401980fbe49fc
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2024-10-20 17:26:36 +0200

    Add session to lualine. Lualine paths should be relative.

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,5 +1,5 @@
 return {
-  { -- Simple and easy statusline.
+  { -- Simple and easy statusline
     "nvim-lualine/lualine.nvim",
     dependencies = {
       -- Useful for getting pretty icons, but requires a Nerd Font.
@@ -9,12 +9,17 @@ return {
       options = {
         -- Set icons_enabled to true if you have a Nerd Font.
         icons_enabled = vim.g.nerd_font_enabled,
-        theme = "onedark",
       },
       sections = {
         lualine_a = { "mode" },
-        lualine_b = { "branch", "diagnostics" },
-        lualine_c = { "filename" },
+        lualine_b = {
+          function()
+            return require("auto-session.lib").current_session_name(true)
+          end,
+          "branch",
+          "diagnostics",
+        },
+        lualine_c = { { "filename", path = 1 } },
         lualine_x = { "encoding", "fileformat", "filetype" },
         lualine_y = { "progress" },
         lualine_z = { "location" },
@@ -22,7 +27,7 @@ return {
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = { "filename" },
+        lualine_c = { { "filename", path = 1 } },
         lualine_x = { "filetype" },
         lualine_y = { "progress" },
         lualine_z = { "location" },
