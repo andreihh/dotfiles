2f4b2980b89af055607908ea64a1fc4b844a09c5
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2024-10-20 17:16:59 +0200

    Add nvim-lint plugin.

---
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,19 @@
+local lsp_opts = require("config.lsp")
+
+return {
+  { -- Linting
+    "mfussenegger/nvim-lint",
+    keys = {
+      {
+        "<leader>l",
+        function()
+          require("lint").try_lint()
+        end,
+        desc = "Lint document and add diagnostics",
+      },
+    },
+    config = function()
+      require("lint").linters_by_ft = lsp_opts.linters_by_ft or {}
+    end,
+  },
+}
