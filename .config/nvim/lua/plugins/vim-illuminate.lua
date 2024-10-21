02db8b1e39261e71e4a37fe01123dff740c1b1a7
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2024-10-21 23:55:35 +0200

    Add vim-illuminate plugin.

---
+++ .config/nvim/lua/plugins/vim-illuminate.lua
@@ -0,0 +1,24 @@
+return {
+  { -- Highlighting and jumping to references
+    "RRethy/vim-illuminate",
+    lazy = false,
+    keys = {
+      {
+        "[r",
+        function()
+          require("illuminate").goto_prev_reference()
+        end,
+        mode = "n",
+        desc = "Jump to previous [R]eference",
+      },
+      {
+        "]r",
+        function()
+          require("illuminate").goto_next_reference()
+        end,
+        mode = "n",
+        desc = "Jump to next [R]eference",
+      },
+    },
+  },
+}
