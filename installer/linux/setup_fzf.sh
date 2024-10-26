d6a6b77cbf967f53dc1d868e5ecabd34c65530c6
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2024-10-27 00:48:56 +0200

    Add archiving tools to package index and document deps in installer scripts.

--- installer/linux/setup_fzf.sh
+++ installer/linux/setup_fzf.sh
@@ -1,22 +1,24 @@
 #!/bin/bash -e
 
 # Installs `fzf` for Linux systems.
+#
+# Requires `git`.
 
 [[ $# -gt 0 ]] && echo "Usage: $0" && exit 1
 
-readonly FZF_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/fzf"
+readonly FZF_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/fzf"
 readonly BIN="${HOME}/.local/bin"
 
 echo "Cleaning up prior 'fzf' installation..."
-rm -rf "${FZF_HOME}"
+rm -rf "${FZF_DIR}"
 
 echo "Downloading 'fzf'..."
-git clone --depth 1 https://github.com/junegunn/fzf.git "${FZF_HOME}"
+git clone --depth 1 https://github.com/junegunn/fzf.git "${FZF_DIR}"
 
 echo "Running 'fzf' installer..."
-"${FZF_HOME}/install" --xdg --bin
+"${FZF_DIR}/install" --xdg --bin
 
 echo "Setting up 'fzf' symlink in user 'bin'..."
-ln -svf "${FZF_HOME}/bin/fzf" "${BIN}/fzf"
+ln -svf "${FZF_DIR}/bin/fzf" "${BIN}/fzf"
 
 echo "Installed 'fzf' successfully!"
--- installer/linux/setup_nerdfont.sh
+++ installer/linux/setup_nerdfont.sh
@@ -1,21 +1,24 @@
 #!/bin/bash -e
 
-# Installs Neovim for Linux systems.
+# Installs `JetBrainsMono` Nerd Font for Linux systems.
+#
+# Requires `wget`, `unzip` and `fontconfig`.
 
 [[ $# -gt 0 ]] && echo "Usage: $0" && exit 1
 
 readonly FONT_ZIP="JetBrainsMono.zip"
-readonly FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$FONT_ZIP"
+readonly FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/${FONT_ZIP}"
+readonly FONTS_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/fonts"
 
 echo "Installing 'JetBrainsMono' font..."
 
 echo "Downloading 'JetBrainsMono'..."
-wget -P "$XDG_DATA_HOME/fonts" "$FONT_URL"
-cd "$XDG_DATA_HOME/fonts"
+wget -P "${FONTS_DIR}" "${FONT_URL}"
+cd "${FONTS_DIR}"
 
 echo "Unpacking 'JetBrainsMono'..."
-unzip "$FONT_ZIP"
-rm "$FONT_ZIP"
+unzip "${FONT_ZIP}"
+rm "${FONT_ZIP}"
 
 echo "Refreshing font cache..."
 fc-cache -fv
