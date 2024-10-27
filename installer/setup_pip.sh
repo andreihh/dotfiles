63d93450551b3e1093e9dd61422ae3c61164f0c5
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2024-10-27 16:01:33 +0100

    Format pip as 'pip' in installation script.

--- installer/setup_pip.sh
+++ installer/setup_pip.sh
@@ -6,6 +6,6 @@
 
 [[ $# -gt 0 ]] && echo "Usage: $0" && exit 1
 
-echo "Installing pip..."
+echo "Installing 'pip'..."
 curl -Lo - https://bootstrap.pypa.io/get-pip.py | python3
-echo "Installed pip successfully!"
+echo "Installed 'pip' successfully!"
