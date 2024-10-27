d0b31b5bbfc9520484cebe6ac56c7a33e292b18f
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2024-10-27 06:19:44 +0100

    Install latest nvm release.

--- installer/setup_nvm.sh
+++ installer/setup_nvm.sh
@@ -7,7 +7,7 @@
 [[ $# -gt 0 ]] && echo "Usage: $0" && exit 1
 
 echo "Installing latest LTS version of 'nvm'..."
-curl -Lo - https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh \
+curl -Lo - https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh \
   | bash
 . "${XDG_CONFIG_HOME:-$HOME/.config}/nvm/nvm.sh"
 nvm install --lts
