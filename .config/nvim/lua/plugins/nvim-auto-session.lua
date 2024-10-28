b3935846c592a4623859074b3beda03dd59c182a
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2024-10-28 01:39:17 +0100

    Add recommended session options for nvim-auto-session.

--- .config/nvim/lua/plugins/nvim-auto-session.lua
+++ .config/nvim/lua/plugins/nvim-auto-session.lua
@@ -1,3 +1,7 @@
+-- Session options recommended for the `auto-session` plugin.
+vim.o.sessionoptions =
+  "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
 return {
   { -- Session management
     "rmagatti/auto-session",
