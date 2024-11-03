a99556f0561b4fdbb04c1adbbdb5d78d020227bf
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2024-11-03 23:18:44 +0100

    Refactor nvim-lspconfig plugin config.

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,17 +1,28 @@
+local lsp_opts = require("config.lsp")
+
 return {
   { -- Main LSP configuration
     "neovim/nvim-lspconfig",
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim.
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
-
-      -- Useful status updates for LSP.
-      "j-hui/fidget.nvim",
+      { "williamboman/mason.nvim", config = true },
+      { "williamboman/mason-lspconfig.nvim", config = true },
+      {
+        -- Ensure the servers and required tools are installed.
+        --  To check the current status of installed tools and/or manually
+        --  install other tools, you can run:
+        --    :Mason
+        --
+        --  You can press `g?` for help in this menu.
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        opts = { ensure_installed = lsp_opts.ensure_installed or {} },
+      },
 
       -- Allows extra capabilities provided by nvim-cmp.
       "hrsh7th/cmp-nvim-lsp",
+
+      -- Useful status updates for LSP.
+      { "j-hui/fidget.nvim", config = true },
     },
     config = function()
       -- LSP servers and clients are able to communicate to each other what
@@ -27,36 +38,17 @@ return {
         require("cmp_nvim_lsp").default_capabilities()
       )
 
-      local lsp_opts = require("config.lsp")
-      local servers = lsp_opts.servers or {}
-
-      -- Ensure the servers and required tools are installed.
-      --  To check the current status of installed tools and/or manually install
-      --  other tools, you can run:
-      --    :Mason
-      --
-      --  You can press `g?` for help in this menu.
-      require("mason").setup()
-      require("mason-tool-installer").setup({
-        ensure_installed = lsp_opts.ensure_installed or {},
-      })
-
-      require("mason-lspconfig").setup_handlers({
-        function(server_name)
-          local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed by the
-          -- server configuration above. Useful when disabling certain
-          -- features of an LSP (for example, turning off formatting for
-          -- ts_ls).
-          server.capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            capabilities,
-            server.capabilities or {}
-          )
-          require("lspconfig")[server_name].setup(server)
-        end,
-      })
+      for server_name, server_config in pairs(lsp_opts.servers or {}) do
+        -- This handles overriding only values explicitly passed by the server
+        -- configuration. Useful when disabling certain features of an LSP (for
+        -- example, turning off formatting for ts_ls).
+        server_config.capabilities = vim.tbl_deep_extend(
+          "force",
+          capabilities,
+          server_config.capabilities or {}
+        )
+        require("lspconfig")[server_name].setup(server_config)
+      end
     end,
   },
 }
