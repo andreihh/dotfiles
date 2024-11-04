78f5cbef798247a3f9fb2b0bc4a9403a520c948b
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2024-11-04 08:41:59 +0100

    Simplify cmp-nvim-lsp capability computation.

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -29,10 +29,9 @@ return { -- LSP configuration
     --  specification. When you add nvim-cmp, luasnip, etc. Neovim now has
     --  *more* capabilities. So, we create new capabilities with nvim cmp, and
     --  then broadcast that to the servers.
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend(
+    local capabilities = vim.tbl_deep_extend(
       "force",
-      capabilities,
+      vim.lsp.protocol.make_client_capabilities(),
       require("cmp_nvim_lsp").default_capabilities()
     )
 
