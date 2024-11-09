01be03758ce76abefc922b8978a9b7376936e55f
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2024-11-09 18:43:54 +0100

    Add line to nvim lsp config to find all available LSPs.

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -17,6 +17,8 @@ local M = {
   --
   --  For example, to see the options for `lua_ls`, you could go to:
   --    https://luals.github.io/wiki/settings/
+  --
+  --  See `:help lspconfig-all` for a list of all the pre-configured LSPs.
   servers = {
     lua_ls = {
       settings = {
