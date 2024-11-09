9b9a37333cc7dc64ac36de62547d582bd7a91509
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2024-11-09 03:46:48 +0100

    Switch nvim-tabline to nvim-tabby.

---
+++ .config/nvim/lua/plugins/nvim-tabby.lua
@@ -0,0 +1,56 @@
+return { -- Decorate tabline with tab index, buffer flags, etc.
+  "nanozuki/tabby.nvim",
+  dependencies = {
+    -- Icons require a Nerd Font.
+    { "nvim-tree/nvim-web-devicons", enabled = vim.g.nerd_font_enabled },
+  },
+  opts = {
+    line = function(line)
+      local theme = {
+        fill = "TabLineFill",
+        head = "TabLine",
+        current_tab = "TabLineSel",
+        tab = "TabLine",
+      }
+      -- Icons require a Nerd Font.
+      local icons = vim.g.nerd_font_enabled
+          and {
+            vim = "  ",
+            seg_start = "",
+            seg_sep = "",
+            seg_end = "",
+            current_seg = "*",
+          }
+        or {
+          vim = " Vim ",
+          seg_start = "",
+          seg_sep = "|",
+          seg_end = " ",
+          current_seg = "*",
+        }
+      return {
+        {
+          { icons.vim, hl = theme.head },
+          line.sep(icons.seg_end, theme.head, theme.fill),
+        },
+        line.tabs().foreach(function(tab)
+          local hl = tab.is_current() and theme.current_tab or theme.tab
+          return {
+            line.sep(icons.seg_start, hl, theme.fill),
+            tab.number(),
+            icons.seg_sep,
+            tab.name(),
+            tab.is_current() and icons.seg_sep or "",
+            tab.is_current() and icons.current_seg or "",
+            line.sep(icons.seg_end, hl, theme.fill),
+            hl = hl,
+            margin = " ",
+          }
+        end),
+        hl = theme.fill,
+      }
+    end,
+    -- Icons require a Nerd Font.
+    option = { nerdfont = vim.g.nerd_font_enabled },
+  },
+}
--- .config/nvim/lua/plugins/nvim-tabline.lua
+++
@@ -1,9 +0,0 @@
-return { -- Decorate tabline with tab index, icon, etc.
-  "crispgm/nvim-tabline",
-  dependencies = {
-    -- Icons require a Nerd Font.
-    { "nvim-tree/nvim-web-devicons", enabled = vim.g.nerd_font_enabled },
-  },
-  -- Icons require a Nerd Font.
-  opts = { show_icon = vim.g.nerd_font_enabled },
-}
