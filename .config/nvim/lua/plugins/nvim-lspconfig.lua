c836a3bd2a9bdbfd25f6f1d24b3f07536d4828be
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2024-11-10 18:58:30 +0100

    Add LspCapabilities nvim command to display attached LSP capabilities.

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,3 +1,12 @@
+vim.api.nvim_create_user_command("LspCapabilities", function()
+  local clients =
+    vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf() })
+  for _, client in pairs(clients) do
+    vim.print("LSP capabilities for server " .. client.name .. ":")
+    vim.print(client.server_capabilities)
+  end
+end, { desc = "Display the attached LSP capabilities" })
+
 return { -- LSP configuration
   "neovim/nvim-lspconfig",
   dependencies = {
