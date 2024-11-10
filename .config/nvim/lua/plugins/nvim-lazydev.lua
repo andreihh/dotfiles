41f4bd086a9f83129ce1122ee7c215d70a4c6be5
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2024-11-10 06:19:05 +0100

    Load Snacks types in nvim lazydev.

--- .config/nvim/lua/plugins/nvim-lazydev.lua
+++ .config/nvim/lua/plugins/nvim-lazydev.lua
@@ -9,6 +9,8 @@ return {
       library = {
         -- Load luvit types when the `vim.uv` word is found.
         { path = "luvit-meta/library", words = { "vim%.uv" } },
+        -- Load snacks types when the `Snacks` word is found.
+        { path = "snacks.nvim", words = { "Snacks" } },
       },
     },
   },
