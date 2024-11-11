93a6ad0dfe2cdfb04283b9e1632dd2582883fa03
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2024-11-11 03:23:51 +0100

    Move vim-signify options into the plugin init function.

--- .config/nvim/lua/plugins/vim-signify.lua
+++ .config/nvim/lua/plugins/vim-signify.lua
@@ -1,6 +1,7 @@
--- Decrease VCS gutter signs priority below diagnostic signs (default 10).
-vim.g.signify_priority = 5
-
 return { -- VCS gutter signs
   "mhinz/vim-signify",
+  init = function()
+    -- Decrease VCS gutter signs priority below diagnostic signs (default 10).
+    vim.g.signify_priority = 5
+  end,
 }
