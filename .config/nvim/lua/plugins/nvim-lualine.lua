6dd3d4b8e6a6ad96d851cd21bf2676c2917cdae5
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2024-11-17 00:37:19 +0100

    Switch nvim tabline from nvim-tabby to nvim-lualine.

--- .config/nvim/lua/plugins/nvim-lualine.lua
+++ .config/nvim/lua/plugins/nvim-lualine.lua
@@ -8,14 +8,17 @@ return { -- Simple and easy statusline
     sections = {
       lualine_a = { "mode" },
       lualine_b = {
-        function()
-          return require("auto-session.lib").current_session_name(true)
-        end,
-        "branch",
+        {
+          function()
+            return require("auto-session.lib").current_session_name(true)
+          end,
+          icon = "󱁿", -- `nf-md-folder-cog`
+        },
+        { "branch", icon = "" }, -- `nf-fa-code_branch`
         "diagnostics",
       },
       lualine_c = { { "filename", path = 1 } },
-      lualine_x = { "encoding", "fileformat", "filetype" },
+      lualine_x = { "filetype" },
       lualine_y = { "progress" },
       lualine_z = { "location" },
     },
@@ -27,12 +30,22 @@ return { -- Simple and easy statusline
       lualine_y = { "progress" },
       lualine_z = { "location" },
     },
+    tabline = {
+      lualine_a = {
+        {
+          "tabs",
+          max_length = vim.o.columns, -- Span entire tab line
+          mode = 2, -- Show tab number and name
+          tabs_color = { active = "TabLineSel", inactive = "TabLine" },
+        },
+      },
+    },
     -- Icons require a Nerd Font.
     options = NerdFontEnabled() and {
       icons_enabled = true,
     } or {
       icons_enabled = false,
-      component_separators = { left = "|", right = "|" },
+      component_separators = { left = "│", right = "│" },
       section_separators = "",
     },
   },
