4ff477a2d7ab39d6240d33565d30bc869fe2cdb6
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2024-11-17 05:00:45 +0100

    Remove superfluous comment from nvim_completion.sh.

--- .config/bash.d/00-nvm_completion.sh
+++ .config/bash.d/00-nvm_completion.sh
@@ -3,6 +3,5 @@
 # shellcheck shell=bash
 # shellcheck source=/dev/null
 
-# Load `nvm` with completion.
 [[ -s "${NVM_DIR}/nvm.sh" ]] && . "${NVM_DIR}/nvm.sh"
 [[ -s "${NVM_DIR}/bash_completion" ]] && . "${NVM_DIR}/bash_completion"
