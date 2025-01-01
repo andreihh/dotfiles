5ab0c32a9b302dc889d3503b2585597a241c0386
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-01-01 16:12:29 +0100

    Adjust nvim auto-session keymap formatting.

--- .config/nvim/lua/plugins/nvim-auto-session.lua
+++ .config/nvim/lua/plugins/nvim-auto-session.lua
@@ -2,21 +2,9 @@ return { -- Session management
   "rmagatti/auto-session",
   lazy = false, -- Ensure auto-loading
   keys = {
-    {
-      "ss",
-      "<cmd>Autosession search<CR>",
-      desc = "[S]ession [S]earch",
-    },
-    {
-      "sD",
-      "<cmd>Autosession delete<CR>",
-      desc = "[S]ession [D]elete",
-    },
-    {
-      "sS",
-      "<cmd>SessionSave<CR>",
-      desc = "[S]ession [S]ave",
-    },
+    { "ss", "<cmd>Autosession search<CR>", desc = "[S]ession [S]earch" },
+    { "sD", "<cmd>Autosession delete<CR>", desc = "[S]ession [D]elete" },
+    { "sS", "<cmd>SessionSave<CR>", desc = "[S]ession [S]ave" },
   },
   -- Don't start sessions in root, home, or their direct children.
   opts = { suppressed_dirs = { "/", "/*", "~/", "~/*" } },
