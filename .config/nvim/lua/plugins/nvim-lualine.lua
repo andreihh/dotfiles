9a41fe9d00e35fe6dd5a38165f17fcac55c80d71
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-01-17 06:32:17 +0100

    Switch (n)vim diagnostic icons from nf-md to nf-fa because they look better (also used by LazyVim).

--- .config/nvim/lua/plugins/nvim-lualine.lua
+++ .config/nvim/lua/plugins/nvim-lualine.lua
@@ -15,7 +15,18 @@ return { -- Simple and easy statusline
           icon = "󱁿", -- `nf-md-folder-cog`
         },
         { "branch", icon = "" }, -- `nf-fa-code_branch`
-        "diagnostics",
+        {
+          "diagnostics",
+          -- Icons require a Nerd Font.
+          symbols = NerdFontEnabled()
+              and {
+                error = " ", -- `nf-fa-times_circle`
+                warn = " ", -- `nf-fa-warning`
+                info = " ", -- `nf-fa-info_circle`
+                hint = " ", -- `nf-fa-lightbulb`
+              }
+            or {},
+        },
       },
       lualine_c = { { "filename", path = 1 } },
       lualine_x = { "filetype" },
