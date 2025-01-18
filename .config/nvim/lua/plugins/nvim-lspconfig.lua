bf975f9f6e65ada27bb5191da0260a1902627f9d
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-01-18 18:20:12 +0100

    Document when to skip nvim blink LSP capabilities update.

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,7 @@
 return { -- LSP configuration
   "neovim/nvim-lspconfig",
   dependencies = {
+    { "j-hui/fidget.nvim", config = true }, -- Useful status updates for LSP
     -- Ensure the servers and required tools are installed.
     --  To check the current status of installed tools and/or manually install
     --  other tools, you can run:
@@ -15,8 +16,6 @@ return { -- LSP configuration
     },
 
     "saghen/blink.cmp", -- Allows extra capabilities provided by `blink.cmp`
-
-    { "j-hui/fidget.nvim", config = true }, -- Useful status updates for LSP
   },
   cond = not vim.tbl_contains(vim.v.argv, "-d"), -- Disable LSPs in diff mode
   opts = { servers = vim.g.lsp.servers },
@@ -30,6 +29,12 @@ return { -- LSP configuration
       --  specification. When you add `blink.cmp`, `luasnip`, etc. Neovim now
       --  has *more* capabilities. So, we create new capabilities with
       --  `blink.cmp`, and then broadcast that to the servers.
+      --
+      -- TODO: skip updating capabilities on Neovim 0.11+ and remove `blink.cmp`
+      -- dependency once https://github.com/neovim/nvim-lspconfig/issues/3494
+      -- is resolved. See:
+      --  - https://github.com/Saghen/blink.cmp/pull/897
+      --  - https://github.com/Saghen/blink.cmp/issues/987
       config.capabilities =
         blink.get_lsp_capabilities(config.capabilities or {})
       lspconfig[server].setup(config)
