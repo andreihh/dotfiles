875bd8afa77d2f9b4750f33bfd0555d220a0d0b5
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-01-19 23:46:13 +0100

    Move setup_intellij.sh to opt/.

--- opt/setup_intellij.sh
+++ opt/setup_intellij.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -e
 #
-# Installs IntelliJ IDEA CE for Linux systems.
+# Installs IntelliJ IDEA CE for Linux or MacOS systems.
 #
-# Requires `snapd`.
+# Requires Homebrew on MacOS.
 
 [[ $# -gt 0 ]] && echo "Usage: $0" && exit 1
+
+echo "Installing IntelliJ IDEA CE..."
+command -v &> /dev/null && brew install --cask intellij-idea-ce
 
 echo "Installing Snap..."
 sudo apt install snapd
@@ -12,6 +15,6 @@ sudo apt install snapd
 echo "Refreshing Snap packages..."
 sudo snap refresh
 
-echo "Installing IntelliJ IDEA CE..."
+echo "Installing IntelliJ IDEA CE Snap package..."
 sudo snap install intellij-idea-community --classic
 echo "Installed IntelliJ IDEA CE successfully!"
