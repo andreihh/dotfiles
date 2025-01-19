4eb5cf1a32108114fa7e70523178b10e17e42c17
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-01-19 20:31:51 +0100

    Simplify Xresources and colorscheme script.

--- .config/profile.d/10-colorscheme.sh
+++ .config/profile.d/10-colorscheme.sh
@@ -4,16 +4,13 @@
 # shellcheck shell=sh
 
 X11_CONFIG_DIR="${XDG_CONFIG_HOME:-${HOME}/.config}/X11"
-X11_COLORSCHEME="${X11_CONFIG_DIR}/colorscheme.Xresources"
 
-[ -d "${X11_CONFIG_DIR}" ] && cat << EOF > "${X11_COLORSCHEME}"
-! colorscheme.Xresources: auto-generated from \`\${COLORSCHEME}\`.
-
-#include "${X11_CONFIG_DIR}/Xcolors/${COLORSCHEME}.Xresources"
-EOF
-
-# Load X11 resources explicitly because `xterm` config is not XDG-compliant.
-command -v xterm > /dev/null 2>&1 && xrdb -merge "${X11_CONFIG_DIR}/Xresources"
+# Load X11 resources explicitly because `xterm` config is not XDG-compliant and
+# cannot import files dynamically.
+command -v xterm > /dev/null 2>&1 \
+  && xrdb -merge "${X11_CONFIG_DIR}/Xresources" \
+  && xrdb -merge "${X11_CONFIG_DIR}/Xcolors/${COLORSCHEME}.Xresources"
 
 unset X11_CONFIG_DIR
-unset X11_COLORSCHEME
+
+
