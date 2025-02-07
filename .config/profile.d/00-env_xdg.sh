7963edaf8adbaccc9baff6b8604bf07e4ebc5491
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-02-07 21:19:46 +0100

    Export more XDG environemnt variables.

--- .config/profile.d/00-env_xdg.sh
+++ .config/profile.d/00-env_xdg.sh
@@ -31,20 +31,29 @@ export GNUPGHOME="${XDG_DATA_HOME}/gnupg"
 # Export `python` history file.
 export PYTHON_HISTORY="${XDG_STATE_HOME}/python_history"
 
+# Export `pyenv` directory.
+export PYENV_ROOT="${XDG_DATA_HOME}/pyenv"
+
 # Export LaTeX directories.
 export TEXMFHOME="${XDG_DATA_HOME}/texmf"
 export TEXMFVAR="${XDG_CACHE_HOME}/texlive/texmf-var"
 export TEXMFCONFIG="${XDG_CONFIG_HOME}/texlive/texmf-config"
 export TEXMACS_HOME_PATH="${XDG_STATE_HOME}/texmacs"
 
+# Export Docker config directory.
+export DOCKER_CONFIG="${XDG_CONFIG_HOME}/docker"
+
 # Export the JRE for apps that rely on `${JAVA_HOME}`.
 # shellcheck disable=SC2155
 export JAVA_HOME="$(readlink -e '/usr/bin/java' | sed 's:/bin/java::')"
 
-# Export local Maven repository.
+# Export Java user preferences location.
+export _JAVA_OPTIONS="-Djava.util.prefs.userRoot='${XDG_CONFIG_HOME}'/java"
+
+# Export Maven local repository.
 export MAVEN_OPTS="-Dmaven.repo.local='${XDG_DATA_HOME}/maven/repository'"
 
-# Export directory for Gradle global config properties, caches, logs, etc.
+# Export Gradle directory for global config properties, caches, logs, etc.
 export GRADLE_USER_HOME="${XDG_DATA_HOME}/gradle"
 
 # Export `nvm` directory.
@@ -53,8 +62,11 @@ export NVM_DIR="${XDG_CONFIG_HOME}/nvm"
 # Export `npm` config file.
 export NPM_CONFIG_USERCONFIG="${XDG_CONFIG_HOME}/npm/npmrc"
 
-# Export Go path.
+# Export Go modules directory.
 export GOPATH="${XDG_DATA_HOME}/go"
 
-# Export Cargo path.
+# Export `rustup` toolchains and configs directory.
+export RUSTUP_HOME="${XDG_DATA_HOME}/rustup"
+
+# Export Cargo local registry.
 export CARGO_HOME="${XDG_DATA_HOME}/cargo"
--- run/linux/setup_ghostty.sh
+++ run/linux/setup_ghostty.sh
@@ -15,8 +15,8 @@ sudo apt install -y libonig5
 
 echo "Checking latest Ghostty release..."
 GHOSTTY_DEB_URL=$(
-   curl -s https://api.github.com/repos/mkasberg/ghostty-ubuntu/releases/latest | \
-   grep -oP "https://github.com/mkasberg/ghostty-ubuntu/releases/download/[^\s/]+/ghostty_[^\s/_]+_amd64_${VERSION_ID}.deb"
+  curl -s https://api.github.com/repos/mkasberg/ghostty-ubuntu/releases/latest \
+    | grep -oP "https://github.com/mkasberg/ghostty-ubuntu/releases/download/[^\s/]+/ghostty_[^\s/_]+_amd64_${VERSION_ID}.deb"
 )
 GHOSTTY_DEB_FILE=$(basename "${GHOSTTY_DEB_URL}")
 
