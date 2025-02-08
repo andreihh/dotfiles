e0814dc1652985c56ec83f21fa0f318d26ab7a20
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-02-08 13:36:59 +0100

    Auto-generate fzf shell integration script during installation.

--- .config/bash.d/00-fzf_completion.sh
+++
@@ -1,8 +0,0 @@
-# fzf_completion.sh: loads `fzf` with completion.
-#
-# shellcheck shell=bash
-
-# Shell keymaps for `fzf`:
-# - <C-r> = search command history
-# - **<tab> = complete path / process id / etc.
-command -v fzf &> /dev/null && eval "$(fzf --bash)"
