31451f29c704a94f1dee23ab932b0a20ecab934e
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-02-08 13:37:20 +0100

    Auto-generated nvm shell integration script during installation.

--- .config/bash.d/00-nvm_completion.sh
+++
@@ -1,7 +0,0 @@
-# nvm_completion.sh: loads `nvm` with completion.
-#
-# shellcheck shell=bash
-# shellcheck source=/dev/null
-
-[[ -s "${NVM_DIR}/nvm.sh" ]] && . "${NVM_DIR}/nvm.sh"
-[[ -s "${NVM_DIR}/bash_completion" ]] && . "${NVM_DIR}/bash_completion"
