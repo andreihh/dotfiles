9d0cb770be9c21a1b53d659a9fe0376317059c89
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-02-08 12:59:18 +0100

    Update Ghostty installer.

--- run/linux/setup_ghostty.sh
+++ run/linux/setup_ghostty.sh
@@ -9,14 +9,15 @@
 echo "Installing Ghostty..."
 
 source /etc/os-release
+ARCH=$(dpkg --print-architecture)
 
 echo "Installing Ghostty dependencies..."
 sudo apt install -y libonig5
 
 echo "Checking latest Ghostty release..."
 GHOSTTY_DEB_URL=$(
-  curl -s https://api.github.com/repos/mkasberg/ghostty-ubuntu/releases/latest \
-    | grep -oP "https://github.com/mkasberg/ghostty-ubuntu/releases/download/[^\s/]+/ghostty_[^\s/_]+_amd64_${VERSION_ID}.deb"
+   curl -s https://api.github.com/repos/mkasberg/ghostty-ubuntu/releases/latest \
+     | grep -oP "https://github.com/mkasberg/ghostty-ubuntu/releases/download/[^\s/]+/ghostty_[^\s/_]+_${ARCH}_${VERSION_ID}.deb"
 )
 GHOSTTY_DEB_FILE=$(basename "${GHOSTTY_DEB_URL}")
 
