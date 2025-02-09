048d860707e6d0581c0cd248653069f7c44384bb
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-02-09 01:47:52 +0100

    Simplify nvim treesitter highlight and indent config (not using Ruby).

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -22,19 +22,12 @@ return {
         "regex",
       },
       auto_install = true, -- Autoinstall languages that are not installed
-      highlight = {
-        enable = vim.g.lsp.treesitter_enabled,
-        -- Some languages depend on vim's regex highlighting system (such as
-        -- Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages
-        --  for indent.
-        additional_vim_regex_highlighting = { "ruby" },
-      },
-      indent = {
-        enable = vim.g.lsp.treesitter_enabled,
-        disable = { "ruby" },
-      },
+      -- Some languages depend on Vim's regex highlighting system (such as Ruby)
+      -- for indent rules. If you are experiencing weird indenting issues, add
+      -- the language to the list of `additional_vim_regex_highlighting` for
+      -- `highlight` and `disable` languages for `indent`.
+      highlight = { enable = vim.g.lsp.treesitter_enabled },
+      indent = { enable = vim.g.lsp.treesitter_enabled },
       -- Select and jump through Treesitter text objects.
       --  See `:help nvim-treesitter-textobjects`
       textobjects = {
