f7baed1eda21209762bf31018915151a727c7ddf
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-02-17 00:01:29 +0100

    Fix nvim lsp_status.

--- .config/nvim/lua/lualine/components/lsp_status.lua
+++ .config/nvim/lua/lualine/components/lsp_status.lua
@@ -36,26 +36,26 @@ function M:init(options)
   -- Apply symbols.
   self.symbols = self.options.symbols or {}
 
-  -- Store LSP progress symbols by client id to use for rendering.
-  self.lsp_progress_by_client_id = {}
+  --- The difference between the `begin` and `end` progress events for each LSP.
+  ---
+  --- @type table<integer, integer>
+  self.lsp_work_by_client_id = {}
 
   vim.api.nvim_create_autocmd("LspProgress", {
     desc = "Update the Lualine LSP status component with progress",
     group = vim.api.nvim_create_augroup("lualine_lsp_progress", {}),
-    ---@param event {data: {client_id: integer, params: lsp.ProgressParams}}
+    --- @param event {data: {client_id: integer, params: lsp.ProgressParams}}
     callback = function(event)
-      -- Set spinner based on current time and update every 100ms.
-      local i = math.floor(vim.uv.hrtime() / 1e8) % #self.symbols.spinner + 1
-      local is_done = event.data.params.value.kind == "end"
-      local prev_progress = self.lsp_progress_by_client_id[event.data.client_id]
-      local next_progress = is_done and self.symbols.done
-        or self.symbols.spinner[i]
+      local kind = event.data.params.value.kind
+      local client_id = event.data.client_id
 
-      -- Update the stored LSP progress symbol.
-      self.lsp_progress_by_client_id[event.data.client_id] = next_progress
+      local work = self.lsp_work_by_client_id[client_id] or 0
+      local work_change = kind == "begin" and 1 or (kind == "end" and -1 or 0)
 
-      -- Refresh Lualine to update the LSP progress symbol if it changed.
-      if prev_progress ~= next_progress then
+      self.lsp_work_by_client_id[client_id] = math.max(work + work_change, 0)
+
+      -- Refresh Lualine to update the LSP status symbol if it changed.
+      if work_change ~= 0 then
         require("lualine").refresh()
       end
     end,
@@ -66,9 +66,18 @@ function M:update_status()
   local bufnr = vim.api.nvim_get_current_buf()
   local result = {}
   for _, client in ipairs(vim.lsp.get_clients({ bufnr = bufnr })) do
-    -- Append the progress to the LSP name only if supported.
-    local progress = self.lsp_progress_by_client_id[client.id]
-    table.insert(result, client.name .. (progress and " " .. progress or ""))
+    local status
+    local work = self.lsp_work_by_client_id[client.id]
+    if work ~= nil and work > 0 then
+      -- Set spinner based on current time and update every 100ms.
+      local i = math.floor(vim.uv.hrtime() / 1e8) % #self.symbols.spinner + 1
+      status = self.symbols.spinner[i]
+    elseif work ~= nil and work == 0 then
+      status = self.symbols.done
+    end
+
+    -- Append the status to the LSP only if it supports progress reporting.
+    table.insert(result, client.name .. (status and " " .. status or ""))
   end
   return table.concat(result, self.symbols.separator)
 end
