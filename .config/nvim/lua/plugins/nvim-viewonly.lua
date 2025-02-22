3f603a502fe7b8fc6af960b00deef7e1224b1a0b
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-02-22 01:08:54 +0100

    Keep nvim view-only plugins sorted.

--- .config/nvim/lua/plugins/nvim-viewonly.lua
+++ .config/nvim/lua/plugins/nvim-viewonly.lua
@@ -1,20 +1,20 @@
 -- Disable plugins for view-only mode (diff, read-only, non-modifiable).
 return (vim.o.diff or vim.o.readonly or not vim.o.modifiable)
-    and {
-      { "rmagatti/auto-session", cond = false },
-      { "stevearc/oil.nvim", cond = false },
-      { "ibhagwan/fzf-lua", cond = false },
+    and { -- Keep sorted
       { "folke/flash.nvim", cond = false },
-      { "kylechui/nvim-surround", cond = false },
-      { "folke/ts-comments.nvim", cond = false },
-      { "neovim/nvim-lspconfig", cond = false },
       { "folke/lazydev.nvim", cond = false },
+      { "folke/ts-comments.nvim", cond = false },
+      { "ibhagwan/fzf-lua", cond = false },
       { "kosayoda/nvim-lightbulb", cond = false },
-      { "stevearc/conform.nvim", cond = false },
+      { "kylechui/nvim-surround", cond = false },
       { "mfussenegger/nvim-lint", cond = false },
+      { "neovim/nvim-lspconfig", cond = false },
+      { "rafamadriz/friendly-snippets", cond = false },
+      { "rmagatti/auto-session", cond = false },
       { "saghen/blink.cmp", cond = false },
       { "saghen/blink.compat", cond = false },
-      { "rafamadriz/friendly-snippets", cond = false },
       { "sindrets/diffview.nvim", cond = false },
+      { "stevearc/conform.nvim", cond = false },
+      { "stevearc/oil.nvim", cond = false },
     }
   or {}
