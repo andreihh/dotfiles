9b6622dadebab3f2c7254dde29c12622bf0175ae
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-03-19 23:34:20 +0100

    Remove custom blink.cmp compat properties.

--- .config/nvim/lua/plugins/nvim-blink.lua
+++ .config/nvim/lua/plugins/nvim-blink.lua
@@ -82,43 +82,6 @@ return {
     -- Allows extending the `sources.default` array elsewhere in your config
     -- without having to redefine it.
     opts_extend = { "sources.default" },
-    -- Extend providers with the following properties:
-    --  @field compat boolean Set up as `blink.compat` source if `true`
-    --  @field kind_name string Use custom completion item kind name
-    --  @field kind_icon string Use custom completion item kind icon
-    config = function(_, opts)
-      for name, provider in pairs(opts.sources.providers or {}) do
-        if provider.compat then
-          -- Set up `blink.compat` source.
-          provider.opts = provider.opts or {}
-          provider.opts.cmp_name = name
-          provider.name = provider.name or name
-          provider.module = "blink.compat.source"
-        end
-
-        if provider.kind_name or provider.kind_icon then
-          -- Register custom completion item kind name and icon for source.
-          local kind_name = provider.kind_name
-          local kind_icon = provider.kind_icon
-          local transform_items = provider.transform_items
-          provider.transform_items = function(ctx, items)
-            items = transform_items and transform_items(ctx, items) or items
-            for _, item in ipairs(items) do
-              item.kind_name = item.kind_name or kind_name
-              item.kind_icon = item.kind_icon or kind_icon
-            end
-            return items
-          end
-        end
-
-        -- Unset custom properties to pass `blink.cmp` validation.
-        provider.compat = nil
-        provider.kind_name = nil
-        provider.kind_icon = nil
-      end
-
-      require("blink.cmp").setup(opts)
-    end,
   },
   { -- Compatibility module for `nvim-cmp` sources used in `blink.cmp`
     "saghen/blink.compat",
