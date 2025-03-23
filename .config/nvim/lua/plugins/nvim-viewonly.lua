cb0119a34da440b5a7c5f4a051f360fcb30c0e1f
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-03-23 23:36:53 +0100

    Fix nvim viewonly condition.

--- .config/nvim/lua/plugins/nvim-viewonly.lua
+++ .config/nvim/lua/plugins/nvim-viewonly.lua
@@ -1,5 +1,10 @@
 -- Disable plugins for view-only mode (diff, read-only, non-modifiable).
-return (vim.o.diff or vim.o.readonly or not vim.o.modifiable)
+local is_viewonly = false
+for _, option in ipairs({ "-d", "-R", "-m", "-M" }) do
+  is_viewonly = is_viewonly or vim.list_contains(vim.v.argv, option)
+end
+
+return is_viewonly
     and { -- Keep sorted
       { "folke/flash.nvim", cond = false },
       { "folke/lazydev.nvim", cond = false },
