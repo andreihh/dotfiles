f41788ae3f5cf54bab954a0388072fbca373074e
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-03-29 19:50:12 +0100

    Update nvim view-only plugins.

--- .config/nvim/lua/plugins/nvim-viewonly.lua
+++ .config/nvim/lua/plugins/nvim-viewonly.lua
@@ -6,6 +6,7 @@ end
 
 return is_viewonly
     and { -- Keep sorted
+      { "WhoIsSethDaniel/mason-tool-installer.nvim", cond = false },
       { "folke/flash.nvim", cond = false },
       { "folke/lazydev.nvim", cond = false },
       { "folke/ts-comments.nvim", cond = false },
@@ -16,8 +17,8 @@ return is_viewonly
       { "rafamadriz/friendly-snippets", cond = false },
       { "rmagatti/auto-session", cond = false },
       { "saghen/blink.cmp", cond = false },
-      { "saghen/blink.compat", cond = false },
       { "sindrets/diffview.nvim", cond = false },
       { "stevearc/conform.nvim", cond = false },
+      { "williamboman/mason.nvim", cond = false },
     }
   or {}
