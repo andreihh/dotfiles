3d9c98f0ad6f9a862a11087cdd2c9fe8136c338a
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-04-14 01:07:35 +0200

    Update Arkenfox user overrides.

--- firefox/user-overrides.js
+++ firefox/user-overrides.js
@@ -5,15 +5,16 @@
 user_pref("browser.startup.page", 3); // 0102
 // user_pref("browser.privatebrowsing.autostart", false); // 0110 required if you had it set as true
 // user_pref("browser.sessionstore.privacy_level", 0); // 1003 optional to restore cookies/formdata
-user_pref("privacy.clearOnShutdown.history", false); // 2811 FF127 or lower
-user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false); // 2811 FF128+
+user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false); // 2811 FF128-135
+user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", false); // 2812 FF136+
 
-// 2820 optional to match when you use settings>Cookies and Site Data>Clear Data
-// user_pref("privacy.clearSiteData.historyFormDataAndDownloads", false); // FF128+
+// optional to match when you use settings>Cookies and Site Data>Clear Data
+// user_pref("privacy.clearSiteData.historyFormDataAndDownloads", false); // 2820 FF128-135
+// user_pref("privacy.clearSiteData.browsingHistoryAndDownloads", false); // 2821 FF136+
 
-// 2830 optional to match when you use Ctrl-Shift-Del (settings>History>Custom Settings>Clear History)
-// user_pref("privacy.cpd.history", false); // FF127 or lower
-// user_pref("privacy.clearHistory.historyFormDataAndDownloads", false); // FF128+
+// optional to match when you use Ctrl-Shift-Del (settings>History>Custom Settings>Clear History)
+// user_pref("privacy.clearHistory.historyFormDataAndDownloads", false); // 2830 FF128-135
+// user_pref("privacy.clearHistory.browsingHistoryAndDownloads", false); // 2831 FF136+
 
 // https://github.com/arkenfox/user.js/wiki/3.2-Overrides-%5BCommon%5D#-security
 // override recipe: enable Google Safe Browsing binary checks for downloads
