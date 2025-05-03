11d4960a6750236aa0c31ba45254d369159996f5
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-05-03 16:04:46 +0200

    Add back support for sonokai theme.

---
+++ .config/fzf/themes/sonokai.sh
@@ -0,0 +1,21 @@
+# sonokai.sh: configures `fzf` theme.
+#
+# shellcheck shell=sh
+
+export FZF_DEFAULT_OPTS="\
+  --color=border:#7f8490 \
+  --color=header:#76cce0 \
+  --color=prompt:#fc5d7c:bold \
+  --color=query:-1 \
+  --color=info:#e7c664 \
+  --color=spinner:#e7c664 \
+  --color=fg:-1 \
+  --color=bg:-1 \
+  --color=hl:#9ed072:bold \
+  --color=fg+:-1 \
+  --color=bg+:#363944 \
+  --color=hl+:#9ed072:bold \
+  --color=gutter:-1 \
+  --color=pointer:#76cce0 \
+  --color=marker:#76cce0 \
+"
