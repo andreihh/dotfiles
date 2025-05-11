479cd4873cc61ef7eed3bb827bb6763f1cd2af5f
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-05-11 13:38:05 +0200

    Override catppuccin cursor to match terminal cursor.
    Revert after https://github.com/catppuccin/nvim/pull/857 is merged.

--- .config/nvim/lua/plugins/nvim-theme.lua
+++ .config/nvim/lua/plugins/nvim-theme.lua
@@ -16,6 +16,8 @@ return { -- Color schemes
           LazyNormal = { fg = colors.text, bg = colors.base },
           MasonNormal = { fg = colors.text, bg = colors.base },
           SnacksNormal = { fg = colors.text, bg = colors.base },
+          -- Override cursor to match terminal.
+          Cursor = { fg = colors.crust, bg = colors.rosewater },
         }
       end,
       integrations = {
