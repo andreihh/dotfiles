40c3f875be0d15d1c27087240a8368a13a24d8be
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-06-07 17:30:28 +0200

    Add opt script to install LaTeX.

---
+++ run/opt/install_latex.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+#
+# Installs LaTeX.
+#
+# Supported systems: Debian, Ubuntu, Fedora, RHEL, MacOS
+# Requirements:
+# - MacOS: Homebrew
+
+[[ $# -gt 0 ]] && echo "Usage: $0" && exit 1
+
+echo "Installing LaTeX..."
+
+command -v apt-get &> /dev/null && sudo apt-get install -y texlive-full
+
+command -v dnf &> /dev/null \
+  && sudo dnf install -y texlive-scheme-full latexmk biber
+
+command -v brew &> /dev/null && brew install --cask mactex biber
+
+echo "Installed LaTeX successfully!"
