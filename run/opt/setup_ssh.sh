98842dec410c042905f9803ce037ed285d708b3e
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-06-07 17:19:54 +0200

    Add setup_ssh.sh opt script.

---
+++ run/opt/setup_ssh.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+#
+# Configures SSH agent forwarding and multiplexing for `CLOUD_HOST`.
+#
+# Supported systems: Debian, Ubuntu, Fedora, RHEL, MacOS
+
+[[ $# -gt 0 ]] && echo "Usage: $0" && exit 1
+
+readonly SSH_CONFIG="${HOME}/.ssh/config"
+readonly SSHD_CONFIG="/etc/ssh/sshd_config"
+
+echo "Ensuring agent forwarding is not disabled..."
+sudo sed -i.bak '/^[ \t]*AllowAgentForwarding[ \t]\+no/d' "${SSHD_CONFIG}"
+sudo rm "${SSHD_CONFIG}.bak"
+
+if [[ -n "${CLOUD_HOST}" ]]; then
+  echo "Appending SSH multiplexing options to SSH config..."
+  cat << EOF >> "${SSH_CONFIG}"
+
+Match host ${CLOUD_HOST}
+  # Required for SSH multiplexing:
+  ControlMaster auto
+  ControlPath ~/.ssh/ctrl-%C
+  ControlPersist yes
+  ForwardAgent yes  # Only for trusted hosts
+EOF
+
+  echo "Configured SSH successfully!"
+fi
