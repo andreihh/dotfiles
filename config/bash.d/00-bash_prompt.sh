57fbe924b1298e4bb727d8a1a6c51ecd9cbc40dd
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-07-14 22:57:45 +0200

    Add PROMPT_COMMAND_CWD and rely on POSIX functions to build the prompt.

--- config/bash.d/00-bash_prompt.sh
+++ config/bash.d/00-bash_prompt.sh
@@ -4,21 +4,32 @@
 #
 # shellcheck shell=bash
 
+# Command used to display the current working directory in the prompt:
+# - Defaults to `prompt_cwd`
+# - If unset, falls back to `pwd`
+# - Override to transform paths
+PROMPT_COMMAND_CWD='prompt_cwd'
+
 # Commands used to detect the current VCS branch to be included in the prompt:
-# - Defaults are `_prompt_git_enabled` and `_prompt_git_branch`
+# - Defaults are `prompt_git_enabled` and `prompt_git_branch`
 # - Unset to drop VCS info from the prompt
 # - Override to support other systems besides `git`
-PROMPT_COMMAND_VCS_ENABLED='_prompt_git_enabled'
-PROMPT_COMMAND_VCS_BRANCH='_prompt_git_branch'
+PROMPT_COMMAND_VCS_ENABLED='prompt_git_enabled'
+PROMPT_COMMAND_VCS_BRANCH='prompt_git_branch'
+
+# Returns the current working directory, collapsing `HOME` to `~`.
+prompt_cwd() {
+  pwd | sed "s:^${HOME}:~:"
+}
 
 # Returns if a `git` repository is detected.
-_prompt_git_enabled() {
-  git rev-parse --is-inside-work-tree &> /dev/null || return 1
+prompt_git_enabled() {
+  git rev-parse --is-inside-work-tree > /dev/null 2>&1 || return 1
 }
 
 # Returns the current `git` branch and dirty status.
-_prompt_git_branch() {
-  _prompt_git_enabled || return 1
+prompt_git_branch() {
+  prompt_git_enabled || return 1
 
   local -r branch="$(git symbolic-ref --quiet --short HEAD 2> /dev/null \
     || git describe --all --exact-match HEAD 2> /dev/null \
@@ -62,23 +73,27 @@ _make_prompt() {
     debian_chroot="$(cat /etc/debian_chroot)"
   fi
 
-  # Set the terminal prompt.
-  PS1="${debian_chroot:+${chroot_style}(${debian_chroot}) }"  # Set `chroot`
-  PS1+="${user_style} \u"  # Set current user, `nf-fa-user`
+  # Set the `chroot`, if any.
+  PS1="${debian_chroot:+${chroot_style}(${debian_chroot}) }"
 
-  # Set the host only if the current session is remote.
+  # Set current user (`nf-fa-user`).
+  PS1="${PS1}${user_style} ${USER}"
+
+  # Set the host only if the current session is remote (`nf-md-monitor`).
   if [[ -n "${SSH_TTY}" ]]; then
-    PS1+="${text_style} at ${host_style}󰍹 \h"  # `nf-md-monitor`
+    PS1="${PS1}${text_style} at ${host_style}󰍹 $(uname -n)"
   fi
 
   # Set current VCS branch if VCS info is enabled and repository is detected.
-  PS1+="${text_style}\$(\${PROMPT_COMMAND_VCS_ENABLED} && echo ' on ')"
-  PS1+="${vcs_style}\$(\${PROMPT_COMMAND_VCS_BRANCH})"
+  PS1="${PS1}${text_style}\$(\${PROMPT_COMMAND_VCS_ENABLED} && echo ' on ')"
+  PS1="${PS1}${vcs_style}\$(\${PROMPT_COMMAND_VCS_BRANCH})"
 
-  PS1+="${text_style} in ${cwd_style} \w"  # Set `cwd`, `nf-fa-folder`
-  PS1+="\n${shell_style}\$ ${reset_style}"  # Set shell and reset style
+  # Set current working directory (`nf-fa-folder`).
+  PS1="${PS1}${text_style} in ${cwd_style} \$(\${PROMPT_COMMAND_CWD:-pwd})"
 
-  PS2="${shell_style}> ${reset_style}"  # Set shell and reset style
+  # Set shell on new line and reset style.
+  PS1="${PS1}$(printf '\n%s ' "${shell_style}\$${reset_style}")"
+  PS2="${shell_style}> ${reset_style}"
 
   # If this is an `xterm`, set the title to `user@host:dir`.
   case "${TERM}" in
