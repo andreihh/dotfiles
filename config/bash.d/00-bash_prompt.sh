74701ff335b13033ec764c955629cd4351c9ddd6
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-07-14 23:31:30 +0200

    Make bash_prompt.sh fully POSIX compliant.

--- config/bash.d/00-bash_prompt.sh
+++ config/bash.d/00-bash_prompt.sh
@@ -1,8 +1,10 @@
 # bash_prompt.sh: configures the shell prompt.
 #
+# The prompt is POSIX compliant: supports `bash`, `dash`, `ksh, and `zsh`.
+#
 # Requires a Nerd Font.
 #
-# shellcheck shell=bash
+# shellcheck shell=sh
 
 # Command used to display the current working directory in the prompt:
 # - Defaults to `prompt_cwd`
@@ -31,17 +33,17 @@ prompt_git_enabled() {
 prompt_git_branch() {
   prompt_git_enabled || return 1
 
-  local -r branch="$(git symbolic-ref --quiet --short HEAD 2> /dev/null \
+  _prompt_branch="$(git symbolic-ref --quiet --short HEAD 2> /dev/null \
     || git describe --all --exact-match HEAD 2> /dev/null \
     || git rev-parse --short HEAD 2> /dev/null \
     || echo '(unknown)')"
 
-  local dirty=''
-  if [[ -n "$(git status --porcelain 2> /dev/null)" ]]; then
-    dirty='*'
+  _prompt_branch_dirty=''
+  if [ -n "$(git status --porcelain 2> /dev/null)" ]; then
+    _prompt_branch_dirty='*'
   fi
 
-  echo " ${branch}${dirty}"  # `nf-fa-code_branch
+  echo " ${_prompt_branch}${_prompt_branch_dirty}"  # `nf-fa-code_branch
 }
 
 # Makes a custom prompt in the following format:
@@ -49,6 +51,11 @@ prompt_git_branch() {
 #
 # The `chroot`, host, and branch are optional. Colors are used if supported.
 _make_prompt() {
+  # Configure style escape sequences (not required for `dash` and `ksh`).
+  _esc1='' && _esc2=''
+  [ -n "${BASH_VERSION}" ] && _esc1='\[' && _esc2='\]'
+  [ -n "${ZSH_VERSION}" ] && _esc1='%{' && _esc2='%}' && setopt prompt_subst
+
   # If colors are supported, define styles and colors:
   # - Text: bold
   # - Shell: green
@@ -57,39 +64,39 @@ _make_prompt() {
   # - Host: green
   # - VCS: yellow
   # - Current working directory: blue
-  if [[ "$(tput colors)" -ge 8 ]]; then
-    local -r reset_style="\[$(tput sgr0)\]"
-    local -r text_style="\[$(tput sgr0 && tput bold)\]"
-    local -r shell_style="\[$(tput sgr0 && tput bold && tput setaf 2)\]"
-    local -r chroot_style="\[$(tput sgr0 && tput bold && tput setaf 5)\]"
-    local -r user_style="\[$(tput sgr0 && tput bold && tput setaf 1)\]"
-    local -r host_style="\[$(tput sgr0 && tput bold && tput setaf 2)\]"
-    local -r vcs_style="\[$(tput sgr0 && tput bold && tput setaf 3)\]"
-    local -r cwd_style="\[$(tput sgr0 && tput bold && tput setaf 4)\]"
+  if [ "$(tput colors)" -ge 8 ]; then
+    _reset_style="${_esc1}$(tput sgr0)${_esc2}"
+    _text_style="${_esc1}$(tput sgr0 && tput bold)${_esc2}"
+    _shell_style="${_esc1}$(tput sgr0 && tput bold && tput setaf 2)${_esc2}"
+    _chroot_style="${_esc1}$(tput sgr0 && tput bold && tput setaf 5)${_esc2}"
+    _user_style="${_esc1}$(tput sgr0 && tput bold && tput setaf 1)${_esc2}"
+    _host_style="${_esc1}$(tput sgr0 && tput bold && tput setaf 2)${_esc2}"
+    _vcs_style="${_esc1}$(tput sgr0 && tput bold && tput setaf 3)${_esc2}"
+    _cwd_style="${_esc1}$(tput sgr0 && tput bold && tput setaf 4)${_esc2}"
   fi
 
   # Set the `chroot` you work in, if any (`nf-fa-folder_tree`).
-  [[ -r /etc/debian_chroot ]] && _debian_chroot="$(cat /etc/debian_chroot)"
-  PS1="${_debian_chroot:+${chroot_style} ${_debian_chroot} }"
+  [ -r /etc/debian_chroot ] && _debian_chroot="$(cat /etc/debian_chroot)"
+  PS1="${_debian_chroot:+${_chroot_style} ${_debian_chroot} }"
 
   # Set current user (`nf-fa-user`).
-  PS1="${PS1}${user_style} ${USER}"
+  PS1="${PS1}${_user_style} ${USER}"
 
   # Set the host only if the current session is remote (`nf-md-monitor`).
-  if [[ -n "${SSH_TTY}" ]]; then
-    PS1="${PS1}${text_style} at ${host_style}󰍹 $(uname -n)"
+  if [ -n "${SSH_TTY}" ]; then
+    PS1="${PS1}${_text_style} at ${_host_style}󰍹 $(uname -n)"
   fi
 
   # Set current VCS branch if VCS info is enabled and repository is detected.
-  PS1="${PS1}${text_style}\$(\${PROMPT_COMMAND_VCS_ENABLED} && echo ' on ')"
-  PS1="${PS1}${vcs_style}\$(\${PROMPT_COMMAND_VCS_BRANCH})"
+  PS1="${PS1}${_text_style}\$(\${PROMPT_COMMAND_VCS_ENABLED} && echo ' on ')"
+  PS1="${PS1}${_vcs_style}\$(\${PROMPT_COMMAND_VCS_BRANCH})"
 
   # Set current working directory (`nf-fa-folder`).
-  PS1="${PS1}${text_style} in ${cwd_style} \$(\${PROMPT_COMMAND_CWD:-pwd})"
+  PS1="${PS1}${_text_style} in ${_cwd_style} \$(\${PROMPT_COMMAND_CWD:-pwd})"
 
   # Set shell on new line and reset style.
-  PS1="${PS1}$(printf '\n%s ' "${shell_style}\$${reset_style}")"
-  PS2="${shell_style}> ${reset_style}"
+  PS1="${PS1}$(printf '\n%s ' "${_shell_style}\$${_reset_style}")"
+  PS2="${_shell_style}> ${_reset_style}"
 }
 
 _make_prompt  # Configure custom prompt
