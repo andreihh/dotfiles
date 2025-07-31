8f7210877cebc2ac7721ace8f206449a7e0fb03a
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-07-31 23:23:39 +0200

    Enable catppuccin.nvim auto-integrations.

--- config/nvim/lua/plugins/nvim-catppuccin.lua
+++ config/nvim/lua/plugins/nvim-catppuccin.lua
@@ -3,13 +3,14 @@ return { -- Catppuccin color scheme
   name = "catppuccin",
   priority = 1000, -- Load before all other plugins
   opts = {
+    auto_integrations = true, -- Auto-enable plugin integrations
     show_end_of_buffer = true,
     styles = { conditionals = {} }, -- Don't use italic for conditionals
     custom_highlights = function(colors)
       return {
         -- Override window separator to match status line.
         WinSeparator = { fg = colors.overlay0, bg = colors.mantle },
-        -- Override float border to match window separator.
+        -- Override float border to match general borders.
         FloatBorder = { fg = colors.overlay0 },
         -- Override background for float windows that have backdrop.
         LazyNormal = { fg = colors.text, bg = colors.base },
@@ -20,11 +21,5 @@ return { -- Catppuccin color scheme
         FzfLuaBorder = { fg = colors.overlay0, bg = colors.base },
       }
     end,
-    integrations = {
-      mason = true,
-      signify = true,
-      snacks = { enabled = true },
-      which_key = true,
-    },
   },
 }
