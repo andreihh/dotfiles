e4c11d81f1800dd820166fdac665853270738eae
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-07-31 23:09:00 +0200

    Update shell aliases.

--- config/sh.d/00-sh_aliases.sh
+++ config/sh.d/00-sh_aliases.sh
@@ -2,26 +2,17 @@
 #
 # shellcheck shell=sh
 
-# Starts a `tmux` session for the current project, trying to find:
-# - The root of the Git repository
-# - The current working directory
-tmxp() {
-  _tmx_project_root="$(git rev-parse --show-toplevel)"
-  [ -z "${_tmx_project_root}" ] && _tmx_project_root="${PWD}"
-  tmx -s "$(basename "${_tmx_project_root}")"
-}
-
 # Open argument with the default program.
 alias o='open > /dev/null 2>&1'
 
 # Enable colors in `less`.
 alias less='less -R'
 
-# Alias `ls` to `lsd` and enable colored output.
-alias ls='lsd --color=always'
-alias la='lsd -A --color=always'
-alias ll='lsd -alF --color=always'
-alias lt='lsd --tree --color=always'
+# Alias `ls` to `lsd` and enable icons and colored output.
+alias ls='lsd --color=always --icon=always'
+alias la='lsd -A --color=always --icon=always'
+alias ll='lsd -alF --color=always --icon=always'
+alias lt='lsd --tree --color=always --icon=always'
 
 # Enable colored `fd` output and show hidden files.
 alias fd='fd --color=always --hidden'
@@ -31,3 +22,6 @@ alias rg='rg --color=always --hidden'
 
 # Enable colored `grep` output.
 alias grep='grep --color=always'
+
+# Enable colored `bat` output.
+alias bat='bat --color=always'
